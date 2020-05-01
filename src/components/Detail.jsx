3059e1abcc0405e62c741643ed188e67fef9083a
Will G Piro <willgpiro@gmail.com>
2020-05-01 11:00:39 -0700

    update detail

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+// import styles from './Details.css';
 
 const Detail = ({ name, image, status, occupation, portrayedby }) => (
   <section>
